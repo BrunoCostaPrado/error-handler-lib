d279efe36369e84a45397c2cc2675e7e65ef6578
Bruno Costa Prado <brunocostaprado@gmail.com>
2025-01-05 23:46:12 -0300

    edit readme

--- build/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare function catchErrorTyped<T, E extends new (message?: string) => Error>(promise: Promise<T>, errorsToCatch?: E[]): Promise<[undefined, T] | [InstanceType<E>]>;
-declare class CustomError extends Error {
-    name: string;
-    extraProp: string;
-}
-export { catchErrorTyped, CustomError };
